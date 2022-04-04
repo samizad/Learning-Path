ee213e05586db0b1c8675fa1db8dfd5603127a05
samizad <34879671+samizad@users.noreply.github.com>
2022-04-04 06:41:55 +0430

    change button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,25 @@
-import ProtoTypes from 'prop-types'
-const Button = ({color, text, onClick}) => {
+import ProtoTypes from 'prop-types';
 
-   
-    return (
-        <div>
-          <button 
-          onClick={onClick} 
-          style={{ backgroundColor:color}} 
-          className='btn'
-          >
-              {text}
-              </button>  
-        </div>
-    )
-}
-Button.defaultProps ={
-    color:'steelblue'
-}
-Button.propTypes={
-    text:ProtoTypes.string,
-    color:ProtoTypes.string,
-    onClick:ProtoTypes.func.isRequired 
-}
+const Button = ({ color, text, onClick }) => {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+Button.defaultProps = {
+  color: 'steelblue'
+};
+Button.propTypes = {
+  text: ProtoTypes.string,
+  color: ProtoTypes.string,
+  onClick: ProtoTypes.func.isRequired
+};
 
-export default Button
+export default Button;
