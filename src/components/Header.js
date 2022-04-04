8525d5a47d59e5c6f07d269c8fd325c282bb7ee7
samizad <34879671+samizad@users.noreply.github.com>
2022-04-04 06:59:35 +0430

    Change Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Button from './Button'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = ({title, onAdd, showAdd}) => {
-   
-    return (
-        <div>
-            <header className='header'>
-                <h1>{title}</h1>
-                <Button color={showAdd?'red': 'green'} text={showAdd?'Close': 'Add'} onClick={onAdd} />
-               
-            </header>
-            
-        </div>
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <div>
+      <header className="header">
+        <h1>{title}</h1>
+      </header>
+    </div>
+  );
+};
 ///style  style={headingStyle} for styling// css in js
-const headingStyle = {
-    color:'red',
-    backgroundColor:'green',
-}
+
 Header.defaultProps = {
-    title:'Task Tracker'
-}
+  title: 'Task Tracker'
+};
 Header.prototypes = {
-    title:PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired
+};
 
-export default Header
-
-
+export default Header;
