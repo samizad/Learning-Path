b4a31895824104e168947bf292b4b20ce96147ef
Sami Zad <samiraghadirzad@gmail.com>
2022-04-05 21:37:18 +0430

    Merge https://github.com/samizad/taskTracker into first-version

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Task from './Task';
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
@@ -17,23 +16,3 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
 };
 
 export default Tasks;
-=======
-
-import Task from './Task'
-  
-const Tasks = ({ tasks, onDelete, onToggle}) => {
-   
-    return (
-        <>
-            {tasks.map((task) =>  (
-                <Task key={task.id} task={task}
-                onDelete={onDelete}
-                onToggle={onToggle}
-                />
-            ))}
-        </>
-    )
-}
-
-export default Tasks
->>>>>>> 8525d5a47d59e5c6f07d269c8fd325c282bb7ee7
